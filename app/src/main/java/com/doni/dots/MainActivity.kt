4034e362b739fbe9af5974e59a712dbf5c057765
Donizete Vida <donizetevida1998@outlook.com>
2020-08-12 11:34:43 -0300

    Create a submodule project and use it

--- app/src/main/java/com/doni/dots/MainActivity.kt
+++ app/src/main/java/com/doni/dots/MainActivity.kt
@@ -3,7 +3,6 @@ package com.doni.dots
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
